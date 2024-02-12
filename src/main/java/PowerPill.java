87528092e8099b9ea85e1df69ad45a2861f5f920
jcampos3 <johana.m.c.3005@gmail.com>
2024-02-11 22:29:48 -0500

    finished

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -10,6 +10,33 @@
 public class PowerPill
 {
     // TODO - replace this line with instruction from step 1
+    public static int DEFAULT_POWER = 10;
+    int power;
+    String name;
+    public PowerPill(String name){
+        power = DEFAULT_POWER;
+        this.name = name;
+    }
+    public PowerPill(String name, int power){
+        this.power = power;
+        this.name = name;
+    }
+    public int getPower(){
+        return power;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setPower(int power){
+        this.power = power;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String toString(){
+        return "PowerPill "+name+" = "+power;
+    }
+
 
     // instance variables
     // TODO - replace this line with instruction from step 2
